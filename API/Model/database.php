005aaa9d72ea8192786ff14d10b5712c6f886803
Cjay Landero <xjaylandero@gmail.com>
2024-03-10 15:47:33 +0800

    merge with main

--- API/Model/database.php
+++ API/Model/database.php
@@ -10,27 +10,24 @@ date_default_timezone_set("Asia/Manila");
 set_time_limit(1000);
 
 //DEFINE CONSTANT SERVER VARIABLES
-define("SERVER", "localhost");
-define("DATABASE", "gc-fams");
-define("USER", "root");
-define("PASSWORD", "");
+define("SERVER", "mysql-gc-fams123.alwaysdata.net");
+define("DATABASE", "gc-fams123_gc-fams");
+define("USER", "351056");
+define("PASSWORD", "GC-F@M$123");
 define("DRIVER", "mysql");
 
 //DOT IS CONCATENATION IN PHP
-    class Connection{
-        private $connectionString = DRIVER.":host=".SERVER.";dbname=".DATABASE."; charset=utf8mb4";
-        private $options = [
-            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-            \PDO::ATTR_EMULATE_PREPARES => false
-        ];
+class Connection
+{
+    private $connectionString = DRIVER . ":host=" . SERVER . ";dbname=" . DATABASE . "; charset=utf8mb4";
+    private $options = [
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES => false
+    ];
 
-        public function connect(){
-            return new \PDO($this->connectionString, USER, PASSWORD, $this->options);
-        }
+    public function connect()
+    {
+        return new \PDO($this->connectionString, USER, PASSWORD, $this->options);
     }
-
-
-
-
-?>
+}
