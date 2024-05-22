aacbdd0bd14b96ed7d91c0c819aa2ebd8ca36b58
Cjay Landero <xjaylandero@gmail.com>
2024-05-22 22:48:19 +0800

    merge w main

--- API/index.php
+++ API/index.php
@@ -48,11 +48,11 @@ if ($request[0] === 'login') {
 //Main request switch endpoints
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        $id = $globalOb->verifyToken()['payload']['id'];
-        $college = $globalOb->verifyToken()['payload']['college'];
+        // $id = $globalOb->verifyToken()['payload']['id'];
+        // $college = $globalOb->verifyToken()['payload']['college'];
 
-        // $id = 40;
-        // $college = 1;
+        $id = 40;
+        $college = 1;
         switch ($request[0]) {
             case 'schedules':
                 $query = $_GET['t'];
